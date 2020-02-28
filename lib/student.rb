c4f181a52d04a18c264a6641ecd7520be3b52632
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-28 16:28:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student < User
   def initialize(first_name, last_name)
     @first_name = first_name 
     @last_name = last_name
-    KNOWLEDGE = []
+    @knowledge = []
   end 
   
   def learn(book_learnin)
